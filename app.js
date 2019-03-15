d17386709d4c2572e1574366242273d7bc466462
yuwenqiang <yuwq@7moor.com>
2019-03-15 18:05:16 +0800

    init object

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+
+app.use(bodyParser.json());
+const routes = require('./routes/index')
+
+app.get('/', routes.routerCheck)
+app.post('/action', routes.routerAction)
+
+app.listen(4000, () => console.log('Example app listening on port 3000!'))
---
+++ routes/index.js
@@ -0,0 +1,101 @@
+/**
+ * @desc router.js
+ * @author yuwenqiang
+ * @date 2019/03/12 14:28:03
+ */
+const dbUrl = "mongodb://localhost:27017/";
+const MongoClient = require('mongodb').MongoClient;
+
+const handel = {
+  /**
+   * @desc 登录
+   * @param {String} id
+   * @param {Object} req
+   * @param {Function} callback
+   * @returns
+   */
+  login (req, callback) {
+    const result = {success: false}
+    MongoClient.connect(dbUrl, { useNewUrlParser: true }, function(err, db) {
+      if (err) throw err
+        db.db('mydb').collection('loginData').findOne({'loginName': req.loginName}, function (err, user) {
+          if (!err) {
+            if (user) {
+              if (req.password === user.password) {
+                result.success = true
+                result.data = {
+                  _id: user._id
+                }
+                result.message = '登录成功'
+                callback(result)
+              } else {
+                result.message = '密码错误'
+                callback(result)
+              }
+            } else {
+              result.message = '该用户未注册'
+              callback(result)
+            }
+          } else {
+            callback(result)
+          }
+        })
+    })
+  },
+  /**
+   * @desc 注册
+   * @param {Object} req
+   * @param {Function} callback
+   * @returns
+   */
+  register (req, callback) {
+    const result = {success: false}
+    MongoClient.connect(dbUrl, { useNewUrlParser: true }, function(err, db) {
+      if (err) throw err
+      // const loginData = db.db('mydb').collection('loginData')
+      const mydb = db.db('mydb');
+      const loginData = mydb.collection('loginData')
+      loginData.findOne({"loginName": req.loginName}, (err, user) => {
+        if (!err) {
+          if (user && user.loginName === req.loginName) {
+            result.message = '账号已存在'
+            callback(result)
+          } else {
+            result.success = true
+            loginData.insertOne({
+              "loginName": req.loginName,
+              "password": req.password
+            })
+            callback(result)
+          }
+        } else {
+          callback(result)
+        }
+      })
+    })
+  },
+  updateUser () {
+  }
+}
+
+module.exports = {
+  routerCheck (req, res) {
+    console.log(req)
+    res.end('hi')
+  },
+  routerAction (req, res) {
+    if (req.body) {
+      const data = JSON.parse(req.body.data)
+      if (req.body.action === 'login') {
+        handel.login(data, (_result) => {
+          res.jsonp(_result)
+        })
+      } else if (req.body.action === 'register') {
+        handel.register(data, (_result) => {
+          res.jsonp(_result)
+        })
+      }
+    }
+  }
+}
+
